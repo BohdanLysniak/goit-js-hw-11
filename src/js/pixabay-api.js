2523af6e1f5edc9292509c836621b823dcda0fc7
BohdanLysniak <marttelo@ukr.net>
2024-03-22 14:17:53 +0200

    task-1

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,14 @@
+export function getImage(query) {
+  const BASE_URL = "https://pixabay.com/api/"
+  const params = new URLSearchParams({
+    key: 43011845 - "ca4f94d8d544ea7c100ad449b",
+    q: query,
+    image_type: "photo",
+    orientation: "horizontal",
+    safesearch: true,
+  })
+
+  const url = `${BASE_URL}?${params}`;
+
+  return fetch(url).then(res => res.json());
+}
---
+++ src/main.js
@@ -0,0 +1,24 @@
+import { getImage } from "./js/pixabay-api"
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+
+const form = document.querySelector(".search-form")
+
+form.addEventListener("submit", sendForm);
+
+function sendForm(event) {
+  event.preventDefault();
+  const inputValue = event.target.elements.search.value.trim();
+  if (inputValue.lenght !== "") {
+    getImage(inputValue);
+  } else {
+    iziToast.show({
+      message: 'Please complete the field!',
+      theme: 'dark',
+      progressBarColor: '#FFFFFF',
+      color: '#EF4040',
+      position: 'topRight',
+    })
+  };
+}
